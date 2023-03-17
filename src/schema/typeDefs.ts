c1bd92ae5503d77dc86f44ec32138abf0c738cc6
jordinhuang <jordinhuang@gmail.com>
2023-03-17 15:13:56 -0400

    added more hero info && ability query in typeDefs

--- src/schema/typeDefs.ts
+++ src/schema/typeDefs.ts
@@ -2,10 +2,20 @@ export const typeDefs = `#graphql
   type Hero {
     name: String
     role: String
+    origin: String
+    bio: String
+    abilities: {
+      name: String
+      description: String
+    }
   }
+
+  input
 
   type Query {
     heroes: [Hero]
     hero(name: String!): Hero
+    ability(name: String!): Hero 
+    
   }
 `
